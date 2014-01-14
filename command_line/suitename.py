cd189a3223152cf3d1b1f0b1483246450ac6def9
jheadd <jheadd@768ed415-1d9b-430c-b6ff-5e308d989102>
2014-01-14 15:30:38 +0000

    added support code for inclusion as a Phenix dependency

---
+++ command_line/suitename.py
@@ -0,0 +1,18 @@
+# LIBTBX_SET_DISPATCHER_NAME phenix.suitename
+
+import libtbx.load_env
+from libtbx.utils import Usage
+import os, sys, subprocess
+
+def run(args):
+  exe = libtbx.env.under_build("suitename/exe/suitename")
+  if (os.name == "nt"):
+    exe += ".exe"
+  if (not os.path.isfile(exe)):
+    from libtbx.str_utils import show_string
+    from libtbx.utils import Sorry
+    raise Sorry("Missing phenix.suitename executable: %s" % show_string(exe))
+  subprocess.call([exe] + args)
+
+if (__name__ == "__main__"):
+  run(sys.argv[1:])
